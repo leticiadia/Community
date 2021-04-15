bdb0413e02067cce400fe9a6788b64377844dffb
Leticia Dias <contatoleticiadia@gmail.com>
2021-04-15 19:59:33 -0300

    js

---
+++ data.js
@@ -0,0 +1,48 @@
+module.exports = [
+    {
+        id: "2alg7MQ6_sI",
+        src: "image/Tela1.jpg",
+        title: "Study with me", 
+        duration: "1 hour", 
+        author: "Leticia Dias",
+        featured: true
+    },
+    {
+        id: "HN1UjzRSdBk",
+        src: "image/Tela2.jpg",
+        title: "Study with me", 
+        duration: "2 hours", 
+        author: "Leticia Dias"
+    },
+    {
+        id: "GykTLqODQuU",
+        src: "image/Tela3.jpg",
+        title: "Study with me", 
+        duration: "3 hours", 
+        author: "Leticia Dias",
+        featured: true
+    },
+    {
+        id: "ghTrp1x_1As",
+        src: "image/Tela4.jpg",
+        title: "Study with me", 
+        duration: "4 hours", 
+        author: "Leticia Dias"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        src: "image/Tela5.jpg",
+        title: "Study with me", 
+        duration: "5 hours", 
+        author: "Leticia Dias",
+        featured: true
+    },
+    {
+        id: "vqrjFnq3-uo",
+        src: "image/Tela6.jpg",
+        title: "Study with me", 
+        duration: "6 hours", 
+        author: "Leticia Dias"
+    }
+
+]
